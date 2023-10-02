9c815694d81ace8aed689b7286f33d74b3b6e8b1
Mark Ridgewell <mridgewell@gmail.com>
2023-10-03 00:45:33 +0200

    Add creation time to commits.json and sort

--- update_commit_list.py
+++ update_commit_list.py
@@ -6,8 +6,10 @@ commits = []
 
 for file in Path('build_times').glob('*.json'):
     with open(file) as f:
-        data = json.load(f)
-    commits += [{'hash': data['run_data']['head_sha']}]
+        data = json.load(f)['run_data']
+    commits += [{'hash': data['head_sha'], 'created_at': data['created_at']}]
+
+commits.sort(key=lambda x: x['created_at'])
 
 with open('commits.json', 'w') as f:
     json.dump({'commits': commits}, f, indent=4)
