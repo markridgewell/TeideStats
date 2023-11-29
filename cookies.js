b67b6335ab52656b7cd7097743d96ee2ddf31621
Mark Ridgewell <mridgewell@gmail.com>
2023-11-29 01:29:59 +0100

    Move cookie functions into separate file

---
+++ cookies.js
@@ -0,0 +1,40 @@
+// Functions from https://javascript.info/cookie#appendix-cookie-functions
+
+// returns the cookie with the given name,
+// or undefined if not found
+function getCookie(name) {
+    const  matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options = {}) {
+    options = {
+        path: '/',
+        SameSite: 'Strict',
+        ...options
+    };
+
+    if (options.expires instanceof Date) {
+        options.expires = options.expires.toUTCString();
+    }
+
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+      updatedCookie += "; " + optionKey;
+      let optionValue = options[optionKey];
+      if (optionValue !== true) {
+        updatedCookie += "=" + optionValue;
+      }
+    }
+
+    document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+    setCookie(name, "", {
+       'max-age': -1
+    })
+}
