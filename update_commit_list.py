5fafced62492368bb14356a3604835516622dd50
Mark <mridgewell@gmail.com>
2026-07-23 01:20:17 +0200

    Optimise graph loading by precalculating build times

--- update_commit_list.py
+++ update_commit_list.py
@@ -1,13 +1,24 @@
 
 from pathlib import Path
 import json
+from datetime import datetime
 
 commits = []
 
 for file in Path('build_times').glob('*.json'):
     with open(file) as f:
         data = json.load(f)['run_data']
-    commits += [{'hash': data['head_sha'], 'created_at': data['created_at']}]
+
+    start_time = data['started_at'] if 'started_at' in data else data['created_at']
+    end_time = data['completed_at'] if 'completed_at' in data else data['updated_at']
+    elapsed_time_s = datetime.fromisoformat(end_time) - datetime.fromisoformat(start_time)
+
+    commits.append({
+        'hash': data['head_sha'],
+        'created_at': data['created_at'],
+        'build_time_s': elapsed_time_s.total_seconds(),
+        'display_title': data['display_title']
+    })
 
 commits.sort(key=lambda x: x['created_at'])
 
